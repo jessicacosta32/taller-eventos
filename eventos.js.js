cf627a9af71f820fdd7b78389ffa43915acd75d4
jessicacosta32 <jessicostaolivera@gmail.com>
2023-08-08 21:10:03 -0300

    Se agrego style y el evento

--- eventos.js.js
+++ eventos.js.js
@@ -1,3 +1,11 @@
 function mostrarSaludo() {
     alert("¡Hola!");
   }
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const botonContainer = document.getElementById("button");
+  
+    botonContainer.addEventListener("click", function () {
+      alert("Hola! Soy el div");
+    });
+  });
